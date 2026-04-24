5b4e5578fb7eb3232a28a5eca5a12a57be9bfeac
ovcarovlev1-star <ovcarovlev1@gmail.com>
2026-04-24 18:11:46 +0200

    Update OyVey.java

--- src/main/java/me/alpha432/oyvey/OyVey.java
+++ src/main/java/me/alpha432/oyvey/OyVey.java
@@ -8,8 +8,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class OyVey implements ModInitializer, ClientModInitializer {
-    public static final String NAME = "OyVey";
-    public static final String VERSION = "0.0.3 - " + SharedConstants.getGameVersion().getName();
+    public static final String NAME = "SoHoLib";
+    public static final String VERSION = "1.0.6" + SharedConstants.getGameVersion().getName();
 
     public static float TIMER = 1f;
 
